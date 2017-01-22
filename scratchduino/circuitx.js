21639dfc16ef88fe9a65352bb9d63a9125e831a8
Hasan Abo-Shally <HasanAboShally@gmail.com>
2017-01-22 21:31:44 +0200

    initial implementation

--- scratchduino/circuitx.js
+++ scratchduino/circuitx.js
@@ -294,11 +294,8 @@
     };
 
     ext.setLed = function (ledIndex, color) {
-        var hexColor = decimalColorToHTMLcolor(color);
-        hexColor = decimalColorToHTMLcolor(color);
-        hexColor = decimalColorToHTMLcolor(color);
-        hexColor = decimalColorToHTMLcolor(color);
-        setNeopixel(ledIndex - 1, hexColor);
+        var hex = decimalColorToHex(color);
+        setNeopixel(ledIndex - 1, hex);
     };
 
     ext.isNoise = function () {
@@ -410,32 +407,38 @@
     }
 
 
-    function decimalColorToHTMLcolor(number) {
-        //converts to a integer
-        var intnumber = number - 0;
+    //function decimalColorToHTMLcolor(number) {
+    //    //converts to a integer
+    //    var intnumber = number - 0;
+    //
+    //    // isolate the colors - really not necessary
+    //    var red, green, blue;
+    //
+    //    // needed since toString does not zero fill on left
+    //    var template = "#000000";
+    //
+    //    // in the MS Windows world RGB colors
+    //    // are 0xBBGGRR because of the way Intel chips store bytes
+    //    red = (intnumber & 0x0000ff) << 16;
+    //    green = intnumber & 0x00ff00;
+    //    blue = (intnumber & 0xff0000) >>> 16;
+    //
+    //    // mask out each color and reverse the order
+    //    intnumber = red | green | blue;
+    //
+    //    // toString converts a number to a hexstring
+    //    var HTMLcolor = intnumber.toString(16);
+    //
+    //    //template adds # for standard HTML #RRGGBB
+    //    HTMLcolor = template.substring(0, 7 - HTMLcolor.length) + HTMLcolor;
+    //
+    //    return HTMLcolor;
+    //}
 
-        // isolate the colors - really not necessary
-        var red, green, blue;
 
-        // needed since toString does not zero fill on left
-        var template = "#000000";
-
-        // in the MS Windows world RGB colors
-        // are 0xBBGGRR because of the way Intel chips store bytes
-        red = (intnumber & 0x0000ff) << 16;
-        green = intnumber & 0x00ff00;
-        blue = (intnumber & 0xff0000) >>> 16;
-
-        // mask out each color and reverse the order
-        intnumber = red | green | blue;
-
-        // toString converts a number to a hexstring
-        var HTMLcolor = intnumber.toString(16);
-
-        //template adds # for standard HTML #RRGGBB
-        HTMLcolor = template.substring(0, 7 - HTMLcolor.length) + HTMLcolor;
-
-        return HTMLcolor;
+    function decimalColorToHex(number) {
+        return "#" + ((number) >>> 0).toString(16).slice(-6);
     }
+
 
 })({});
